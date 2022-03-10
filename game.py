78c89c9d5b90bc807118e90b6f9f699e7b608f05
Marie Engel <marie.engel@deliveryhero.com>
2022-03-10 14:04:02 +0100

    added evaluate_guess func

--- game.py
+++ game.py
@@ -1,37 +1,47 @@
-#from colorama import init, Back
+# from colorama import init, Back
 import random
 from words import word_list
 
-#init()
+# init()
+def evaluate_guess(to_guess, guessed):
+    evaluated = ""
+    for i in range(len(to_guess)):
+        if guessed[i] == to_guess[i]:
+            evaluated = evaluated + guessed[i]
+        else:
+            evaluated = evaluated + "*"
+
+    return evaluated
+
+
 def main():
     loop = True
     while loop:
-        print("Would you like to start a new game? Type 'yes' to start, type 'no' to exit.")
+        print(
+            "Would you like to start a new game? Type 'yes' to start, type 'no' to exit."
+        )
         start_input = input()
         if start_input == "no":
             loop = False
-        elif start_input == 'yes':
+        elif start_input == "yes":
             round_count = 0
             word_list_trial = ["cigar"]
-            #to_guess = random.choice(word_list_trial)
+            # to_guess = random.choice(word_list_trial)
             to_guess = "cigar"
             print("Enter a word of 5 letters")
             while round_count < 6:
                 guessed = input()
-                evaluated = ""
-                for i in range(len(to_guess)):
-                    if guessed[i] == to_guess[i]:  # correct letter at correct position
-                        evaluated = evaluated + guessed[i]
-                    else:  # wrong letter
-                        evaluated = evaluated + '*'
-                print(evaluated)
-                if guessed == to_guess: #  if the guess is correct at the first try, we don't need the  for loop...? 
+                print(evaluate_guess(to_guess, guessed))
+                if (
+                    guessed == to_guess
+                ):  #  if the guess is correct at the first try, we don't need the  for loop...?
                     print("You guessed correctly!")
                     break
                 round_count = round_count + 1
         else:
-            print("You had a typo. Would you like to start a new game? Type 'yes' to start, type 'no' to exit.")    
-
+            print(
+                "You had a typo. Would you like to start a new game? Type 'yes' to start, type 'no' to exit."
+            )
 
 
 if __name__ == "__main__":
