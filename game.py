a76e82cbaa353afac6699d242799b30fa6e9f47b
Marie Engel <marie.engel@deliveryhero.com>
2022-03-10 14:59:52 +0100

    added get_word func for random word

--- game.py
+++ game.py
@@ -3,6 +3,11 @@ import random
 from words import word_list
 
 # init()
+def get_word(word_list):
+    to_guess = random.choice(word_list)
+    return to_guess
+
+
 def evaluate_guess(to_guess, guessed):
     evaluated = ""
     for i in range(len(to_guess)):
@@ -15,26 +20,21 @@ def evaluate_guess(to_guess, guessed):
 
 
 def main():
-    loop = True
-    while loop:
+    while True:
         print(
             "Would you like to start a new game? Type 'yes' to start, type 'no' to exit."
         )
         start_input = input()
         if start_input == "no":
-            loop = False
+            break
         elif start_input == "yes":
             round_count = 0
-            word_list_trial = ["cigar"]
-            # to_guess = random.choice(word_list_trial)
-            to_guess = "cigar"
+            to_guess = get_word(word_list)
             print("Enter a word of 5 letters")
             while round_count < 6:
                 guessed = input()
                 print(evaluate_guess(to_guess, guessed))
-                if (
-                    guessed == to_guess
-                ):  #  if the guess is correct at the first try, we don't need the  for loop...?
+                if guessed == to_guess:
                     print("You guessed correctly!")
                     break
                 round_count = round_count + 1
