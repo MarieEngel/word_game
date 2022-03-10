15b574f019f7a87d9a3027d5e9e56992d476d7fb
Marie Engel <marie.engel@deliveryhero.com>
2022-03-10 19:37:43 +0100

    added color to evaluated letters

--- game.py
+++ game.py
@@ -2,21 +2,20 @@ from colorama import Back
 import random
 from words import word_list
 
+
 def create_game(word_list):
-    return {
-        "word": random.choice(word_list),
-        "guesses": 6
-    }
+    return {"word": random.choice(word_list), "guesses": 6}
 
 
 def evaluate_guess(to_guess, guessed):
     evaluated = ""
     for i in range(len(to_guess)):
         if guessed[i] == to_guess[i]:
-            evaluated = evaluated + guessed[i]
+            evaluated = evaluated + Back.GREEN + guessed[i] + Back.RESET
+        elif guessed[i] in to_guess:
+            evaluated = evaluated + Back.BLUE + guessed[i] + Back.RESET
         else:
-            evaluated = evaluated + "*"
-
+            evaluated = evaluated + guessed[i]
     return evaluated
 
 
@@ -32,13 +31,13 @@ def main():
             game = create_game(word_list)
 
             print("Enter a word of 5 letters")
-            while game['guesses'] > 0:
+            while game["guesses"] > 0:
                 guessed = input()
-                print(Back.GREEN + evaluate_guess(game['word'], guessed))
-                if guessed == game['word']:
+                print(evaluate_guess(game["word"], guessed))
+                if guessed == game["word"]:
                     print("You guessed correctly!")
                     break
-                game['guesses'] -= 1
+                game["guesses"] -= 1
         else:
             print(
                 "You had a typo. Would you like to start a new game? Type 'yes' to start, type 'no' to exit."
